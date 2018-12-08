bd6fc061758b3200eda1997cb5a12ac9f38af6d6
Juan Casado Ballesteros <juan.casado@edu.uah.es>
2018-12-08 18:39:17 +0100

    perfomance

--- src/GrafoJSON.java
+++ src/GrafoJSON.java
@@ -15,6 +15,8 @@ public class GrafoJSON {
     private HashMap<String, String> ultimaProperty;
     private String ultimaPropertyId;
 
+    private String dot;
+
     public void addUltimo(int num) {
         ultimoIntroducido = num;
     }
@@ -199,139 +201,144 @@ public class GrafoJSON {
             ArrayList<String> _node_class, ArrayList<String> _edge_class, ArrayList<String> _node_property,
             ArrayList<String> _edge_property, ArrayList<String> _node_inheritance, ArrayList<String> _edge_inheritance,
             ArrayList<String> _node_indirect_use, ArrayList<String> _edge_indirect_use) {
-        StringBuffer sb = new StringBuffer();
-        // Defecto
-        sb.append("graph ejemplo{\n");
-        sb.append("//Defecto\n");
-        sb.append("node [fontname=\"Arial\"];\n");
-        sb.append("edge [fontname=\"Arial\",fontsize=12];\n");
+        if (dot==null){
+            StringBuffer sb = new StringBuffer();
+            // Defecto
+            sb.append("graph ejemplo{\n");
+            sb.append("//Defecto\n");
+            sb.append("node [fontname=\"Arial\"];\n");
+            sb.append("edge [fontname=\"Arial\",fontsize=12];\n");
 
-        // Propiedades
-        sb.append("\n//PROPERTIES\n");
-        sb.append(fragmentDot(_node_property, "node"));
-        sb.append(fragmentDot(_edge_property, "edge"));
-        for (String propertyID : bufferProperties.keySet()) {
-            HashMap<String, String> propertyContent = bufferProperties.get(propertyID);
-            if (propertyContent.containsKey("name")) {
-                sb.append("property_");
-                if (propertyContent.containsKey(languaje)){
-                    sb.append(propertyContent.get(languaje));
-                    sb.append(" [label=\"{" + propertyContent.get(languaje) + "|");
-                }else{
-                    sb.append(propertyContent.get("name"));
-                    sb.append(" [label=\"{" + propertyContent.get("name") + "|");
-                }
-                if (propertyContent.containsKey("typeOf"))
-                    sb.append(propertyContent.get("typeOf"));
-                sb.append("}\"");
-                if (propertyContent.containsKey("optional")){
-                    if (propertyContent.get("optional").equals("true")){
-                        sb.append(",style=\"filled,dashed\"");
+            // Propiedades
+            sb.append("\n//PROPERTIES\n");
+            sb.append(fragmentDot(_node_property, "node"));
+            sb.append(fragmentDot(_edge_property, "edge"));
+            for (String propertyID : bufferProperties.keySet()) {
+                HashMap<String, String> propertyContent = bufferProperties.get(propertyID);
+                if (propertyContent.containsKey("name")) {
+                    sb.append("property_");
+                    if (propertyContent.containsKey(languaje)){
+                        sb.append(propertyContent.get(languaje));
+                        sb.append(" [label=\"{" + propertyContent.get(languaje) + "|");
+                    }else{
+                        sb.append(propertyContent.get("name"));
+                        sb.append(" [label=\"{" + propertyContent.get("name") + "|");
                     }
-                }
-                sb.append("];\n");
-            }
-        }
-
-        //Clases
-        sb.append("\n//CLASES\n");
-        sb.append(fragmentDot(_node_class, "node"));
-        sb.append(fragmentDot(_edge_class, "edge"));
-        for (String claseID : _clases.keySet()){
-            String nombreClase = _clases.get(claseID).getName(languaje);
-            sb.append("class_");
-            sb.append(nombreClase);
-            sb.append(" [label=");
-            sb.append(nombreClase);
-            sb.append("];\n");
-        }
-
-        //Relaciones
-        sb.append("\n//RELACIONES\n");
-        sb.append(fragmentDot(_node_relationship, "node"));
-        for (RelacionJSON relacion : _relaciones) {
-            if (relacion.hasID()){
-                String nombreRealcion = relacion.getName(languaje);
-                if (nombreRealcion!=null){
-                    sb.append("relationship_");
-                    sb.append(nombreRealcion);
-                    sb.append(" [label=");
-                    sb.append(nombreRealcion);
+                    if (propertyContent.containsKey("typeOf"))
+                        sb.append(propertyContent.get("typeOf"));
+                    sb.append("}\"");
+                    if (propertyContent.containsKey("optional")){
+                        if (propertyContent.get("optional").equals("true")){
+                            sb.append(",style=\"filled,dashed\"");
+                        }
+                    }
                     sb.append("];\n");
                 }
             }
-        }
 
+            //Clases
+            sb.append("\n//CLASES\n");
+            sb.append(fragmentDot(_node_class, "node"));
+            sb.append(fragmentDot(_edge_class, "edge"));
+            for (String claseID : _clases.keySet()){
+                String nombreClase = _clases.get(claseID).getName(languaje);
+                sb.append("class_");
+                sb.append(nombreClase);
+                sb.append(" [label=");
+                sb.append(nombreClase);
+                sb.append("];\n");
+            }
 
-        //Clase -- Propiedad
-        sb.append("\n//CLASE -- PROPIEDAD\n");
-        sb.append(fragmentDot(_edge_relationship, "edge"));
-        sb.append("\n");
-        for (String claseID : _clases.keySet()){
-            ClaseJSON clase = _clases.get(claseID);
-            for (int i = 0; i<clase.amountProperties();i++){
-                if (bufferProperties.containsKey(clase.getProperty(i))) {
-                    sb.append("class_");
-                    sb.append(clase.getName(languaje));
-                    sb.append( " -- ");
-                    sb.append("property_");
-                    if (bufferProperties.get(clase.getProperty(i)).containsKey(languaje)){
-                        sb.append(bufferProperties.get(clase.getProperty(i)).get(languaje));
-                    }else{
-                        sb.append(bufferProperties.get(clase.getProperty(i)).get("name"));
+            //Relaciones
+            sb.append("\n//RELACIONES\n");
+            sb.append(fragmentDot(_node_relationship, "node"));
+            for (RelacionJSON relacion : _relaciones) {
+                if (relacion.hasID()){
+                    String nombreRealcion = relacion.getName(languaje);
+                    if (nombreRealcion!=null){
+                        sb.append("relationship_");
+                        sb.append(nombreRealcion);
+                        sb.append(" [label=");
+                        sb.append(nombreRealcion);
+                        sb.append("];\n");
                     }
-                    sb.append("\n");
                 }
             }
-        }
 
-        //Relacion -- Propiedad
-        sb.append("//RELACION -- PROPIEDAD\n");
-        for (RelacionJSON relacion : _relaciones) {
-            for (int i = 0; i < relacion.amountProperties(); i++) {
-                if (relacion.getName(languaje)!=null){
-                    if (bufferProperties.containsKey(relacion.getProperty(i))) {
-                        sb.append("relationship_");
-                        sb.append(relacion.getName(languaje));
-                        sb.append(" -- ");
+
+            //Clase -- Propiedad
+            sb.append("\n//CLASE -- PROPIEDAD\n");
+            sb.append(fragmentDot(_edge_relationship, "edge"));
+            sb.append("\n");
+            for (String claseID : _clases.keySet()){
+                ClaseJSON clase = _clases.get(claseID);
+                for (int i = 0; i<clase.amountProperties();i++){
+                    if (bufferProperties.containsKey(clase.getProperty(i))) {
+                        sb.append("class_");
+                        sb.append(clase.getName(languaje));
+                        sb.append( " -- ");
                         sb.append("property_");
-                        if (bufferProperties.get(relacion.getProperty(i)).containsKey(languaje)) {
-                            sb.append(bufferProperties.get(relacion.getProperty(i)).get(languaje));
-                        } else {
-                            sb.append(bufferProperties.get(relacion.getProperty(i)).get("name"));
+                        if (bufferProperties.get(clase.getProperty(i)).containsKey(languaje)){
+                            sb.append(bufferProperties.get(clase.getProperty(i)).get(languaje));
+                        }else{
+                            sb.append(bufferProperties.get(clase.getProperty(i)).get("name"));
                         }
                         sb.append("\n");
                     }
                 }
             }
-        }
 
-        // uso indirecto de definición de tipos
-        sb.append(fragmentDot(_node_indirect_use, "node"));
-        sb.append(fragmentDot(_edge_indirect_use, "edge"));
-        sb.append("// uso indirecto de definición de tipos\n");
-        for (String idProperty : bufferProperties.keySet()){
-            if (bufferProperties.get(idProperty).containsKey("typeOf")){
-                if (_clases.keySet().contains(bufferProperties.get(idProperty).get("typeOf"))){
-                    sb.append("property_");
-                    if (bufferProperties.get(idProperty).containsKey(languaje)) {
-                        sb.append(bufferProperties.get(idProperty).get(languaje));
-                    } else {
-                        sb.append(bufferProperties.get(idProperty).get("name"));
+            //Relacion -- Propiedad
+            sb.append("//RELACION -- PROPIEDAD\n");
+            for (RelacionJSON relacion : _relaciones) {
+                for (int i = 0; i < relacion.amountProperties(); i++) {
+                    if (relacion.getName(languaje)!=null){
+                        if (bufferProperties.containsKey(relacion.getProperty(i))) {
+                            sb.append("relationship_");
+                            sb.append(relacion.getName(languaje));
+                            sb.append(" -- ");
+                            sb.append("property_");
+                            if (bufferProperties.get(relacion.getProperty(i)).containsKey(languaje)) {
+                                sb.append(bufferProperties.get(relacion.getProperty(i)).get(languaje));
+                            } else {
+                                sb.append(bufferProperties.get(relacion.getProperty(i)).get("name"));
+                            }
+                            sb.append("\n");
+                        }
                     }
-                    sb.append(" -- ");
-                    sb.append("class_");
-                    sb.append(_clases.get(bufferProperties.get(idProperty).get("typeOf")).getName(languaje));
-                    sb.append(" [label=\"TypeOf\"];\n");
                 }
             }
+
+            // uso indirecto de definición de tipos
+            sb.append(fragmentDot(_node_indirect_use, "node"));
+            sb.append(fragmentDot(_edge_indirect_use, "edge"));
+            sb.append("// uso indirecto de definición de tipos\n");
+            for (String idProperty : bufferProperties.keySet()){
+                if (bufferProperties.get(idProperty).containsKey("typeOf")){
+                    if (_clases.keySet().contains(bufferProperties.get(idProperty).get("typeOf"))){
+                        sb.append("property_");
+                        if (bufferProperties.get(idProperty).containsKey(languaje)) {
+                            sb.append(bufferProperties.get(idProperty).get(languaje));
+                        } else {
+                            sb.append(bufferProperties.get(idProperty).get("name"));
+                        }
+                        sb.append(" -- ");
+                        sb.append("class_");
+                        sb.append(_clases.get(bufferProperties.get(idProperty).get("typeOf")).getName(languaje));
+                        sb.append(" [label=\"TypeOf\"];\n");
+                    }
+                }
+            }
+
+
+            sb.append("\n}\n");
+
+            //System.out.println(sb.toString());
+            dot = sb.toString();
+            return sb.toString();
+        }else {
+            return dot;
         }
-
-
-        sb.append("\n}\n");
-
-        //System.out.println(sb.toString());
-        return sb.toString();
     }
 
     private String fragmentDot(ArrayList<String> cadena, String tipo) {
--- src/transformacion.java
+++ src/transformacion.java
@@ -18,7 +18,7 @@ public class transformacion {
                 treeEntrada = procesarEntrada(System.in); // cuando no nos pasan argumento
             }
             walker.walk(listenerEntrada, treeEntrada);
-
+            System.out.println(tablaEntrada.toString());
             ArrayList<String> _node_relationship = tablaEntrada.getNodeRelationshipParameters();
             ArrayList<String> _edge_relationship = tablaEntrada.getEdgeRelationshipParameters();
             ArrayList<String> _node_class = tablaEntrada.getNodeClassParameters();
@@ -29,18 +29,10 @@ public class transformacion {
             ArrayList<String> _edge_inheritance = tablaEntrada.getEdgeInheritanceParameters();
             ArrayList<String> _node_indirect_use = tablaEntrada.getNodeIndirectUseParameters();
             ArrayList<String> _edge_indirect_use = tablaEntrada.getEdgeIndirectUseParameters();
-
-            System.out.println(tablaEntrada.toString());
-            System.out.println("----------------------------------------------------------------------------");
-            System.out.println("Parameters:");
-            System.out.println("----------------------------------------------------------------------------");
-            processJSON(tablaEntrada, walker, "name",_node_relationship, _edge_relationship, _node_class, _edge_class,
-                    _node_property, _edge_property, _node_inheritance, _edge_inheritance, _node_indirect_use,
-                    _edge_indirect_use);
-            System.out.println("----------------------------------------------------------------------------");
+            ArrayList<ActionTable> actions = new ArrayList<>();
+            String languaje = "name";
+            actions.add(tablaEntrada);
             if (tablaEntrada.hasCSV()) {
-                System.out.println("CSV:");
-                System.out.println("----------------------------------------------------------------------------");
                 for (int i = 0; i < tablaEntrada.csvSize(); i++) {
                     tablaEntrada.getCSV(i);
                     try {
@@ -48,16 +40,97 @@ public class transformacion {
                         CSVListener listenerCSV = new CSVListener(tablaCSV);
                         ParseTree treeCSV = procesarCSV(new FileInputStream(tablaEntrada.getCSV(i)));
                         walker.walk(listenerCSV, treeCSV);
-
-                        processJSON(tablaCSV, walker, "name",_node_relationship, _edge_relationship, _node_class, _edge_class,
-                                _node_property, _edge_property, _node_inheritance, _edge_inheritance,
-                                _node_indirect_use, _edge_indirect_use);
+                        actions.add(tablaCSV);
                     } catch (Exception e) {
                         System.out.println("ERROR al procesar el archivo CSV: " + tablaEntrada.getCSV(i));
                         System.out.println(e.toString());
                     }
                 }
-                System.out.println("----------------------------------------------------------------------------");
+            }
+            HashMap<String,JSONTable> jsonResult= new HashMap<>();
+            for (ActionTable at : actions){
+                for (int i = 0; i < at.size(); i++) {
+                    Action action = at.toDo(i);
+                    if (action != Action.skip) {
+                        try {
+                            System.out.println("Procesando JSON " + at.get(i, Content.json));
+                            JSONTable tablaJSON;
+                            if (jsonResult.keySet().contains(at.get(i, Content.json))){
+                                tablaJSON = jsonResult.get(at.get(i, Content.json));
+                            }else{
+                                tablaJSON = new JSONTable();
+                                jsonResult.put(at.get(i, Content.json), tablaJSON);
+                                JSONListener listenerJSON = new JSONListener(tablaJSON);
+                                ParseTree treeJSON = procesarJSON(new FileInputStream(at.get(i, Content.json)));
+                                walker.walk(listenerJSON, treeJSON);
+                                // System.out.println(tablaJSON.toString());
+                            }
+                            for (int j = 0; j < tablaJSON.getSize(); j++) {
+                                String dotContent = tablaJSON.getDotContent(j, languaje, _node_relationship,
+                                        _edge_relationship, _node_class, _edge_class, _node_property, _edge_property,
+                                        _node_inheritance, _edge_inheritance, _node_indirect_use, _edge_indirect_use);
+                                Engine engine = tablaJSON.getEngine(j);
+                                String dotName = at.get(i, Content.dot);
+                                String svgName = at.get(i, Content.svg);
+                                System.out.println("Found : " + tablaJSON.getName(j));
+                                if (tablaJSON.getSize() > 1) {
+                                    if (dotName.endsWith(".dot")) {
+                                        dotName = dotName.replace(".dot", "_" + tablaJSON.getName(j) + ".dot");
+                                    } else {
+                                        dotName += "_" + tablaJSON.getName(j) + ".dot";
+                                    }
+                                    if (svgName.endsWith(".svg")) {
+                                        svgName = svgName.replace(".svg", "_" + tablaJSON.getName(j) + ".svg");
+                                    } else {
+                                        svgName += "_" + tablaJSON.getName(j) + ".svg";
+                                    }
+                                }
+                                if (action == Action.saveSvg) {
+                                    // AQUI GUARDAMOS EL SVG
+                                    try {
+                                        GraphGenerator.generateGraphFromFileTmp(engine, dotContent, svgName);
+                                        System.out.println("Guardando SVG " + svgName + " with " + engine.toString());
+                                    } catch (Exception e) {
+                                        System.out.println("Error al guardar el SVG " + svgName);
+                                        System.out.println(e.toString());
+                                    }
+                                } else if (action == Action.saveDot) {
+                                    // AQUI GUARDAMOS EL DOT
+                                    try {
+                                        FileManager.write(dotName, dotContent);
+                                        System.out.println("Guardando DOT " + dotName);
+                                    } catch (Exception e) {
+                                        System.out.println("Error al guardar el DOT " + dotName);
+                                        System.out.println(e.toString());
+                                    }
+
+                                } else if (action == Action.saveBoth) {
+                                    // AQUI GUARDAMOS AMBOS
+                                    try {
+                                        // System.out.println("Guardando AMBOS");
+                                        System.out.println("Guardando DOT " + dotName);
+                                        FileManager.write(dotName, dotContent);
+                                    } catch (Exception e) {
+                                        System.out.println("Error al guardar el DOT " + dotName);
+                                        System.out.println(e.toString());
+                                    }
+                                    try {
+                                        System.out.println("Guardando SVG " + svgName + " with " + engine.toString());
+                                        GraphGenerator.generateGraphFromFile(engine, dotName, svgName);
+                                    } catch (Exception e) {
+                                        System.out.println("Error al guardar el SVG " + svgName);
+                                        System.out.println(e.toString());
+                                    }
+                                }
+                            }
+                            // System.out.println(tablaJSON.toString());
+                        
+                        } catch (Exception e) {
+                            System.out.println("ERROR al procesar el archivo JSON " + at.get(i, Content.json));
+                            System.out.println(e.toString());
+                        }
+                    }
+                }
             }
         } catch (Exception e) {
             System.out.println("ERROR al procesar la ENTRADA");
@@ -130,89 +203,6 @@ public class transformacion {
         JSONParser parserJSON = new JSONParser(new CommonTokenStream(new JSONLexer(CharStreams.fromStream(datos))));
         parserJSON.setBuildParseTree(true);
         return parserJSON.init();
-    }
-
-    public static void processJSON(ActionTable at, ParseTreeWalker walker, String languaje,ArrayList<String> _node_relationship,
-            ArrayList<String> _edge_relationship, ArrayList<String> _node_class, ArrayList<String> _edge_class,
-            ArrayList<String> _node_property, ArrayList<String> _edge_property, ArrayList<String> _node_inheritance,
-            ArrayList<String> _edge_inheritance, ArrayList<String> _node_indirect_use,
-            ArrayList<String> _edge_indirect_use) {
-        for (int i = 0; i < at.size(); i++) {
-            Action action = at.toDo(i);
-            if (action != Action.skip) {
-                try {
-                    System.out.println("Procesando JSON " + at.get(i, Content.json));
-                    JSONTable tablaJSON = new JSONTable();
-                    JSONListener listenerJSON = new JSONListener(tablaJSON);
-                    ParseTree treeJSON = procesarJSON(new FileInputStream(at.get(i, Content.json)));
-                    walker.walk(listenerJSON, treeJSON);
-
-                    //System.out.println(tablaJSON.toString());
-                    for (int j = 0; j < tablaJSON.getSize(); j++) {
-                        String dotContent = tablaJSON.getDotContent(j, languaje,_node_relationship, _edge_relationship,
-                                _node_class, _edge_class, _node_property, _edge_property, _node_inheritance,
-                                _edge_inheritance, _node_indirect_use, _edge_indirect_use);
-                        Engine engine = tablaJSON.getEngine(j);
-                        String dotName = at.get(i, Content.dot);
-                        String svgName = at.get(i, Content.svg);
-                        System.out.println("Found : " + tablaJSON.getName(j));
-                        if (tablaJSON.getSize() > 1) {
-                            if (dotName.endsWith(".dot")) {
-                                dotName = dotName.replace(".dot", "_" + tablaJSON.getName(j) + ".dot");
-                            } else {
-                                dotName += "_" + tablaJSON.getName(j) + ".dot";
-                            }
-                            if (svgName.endsWith(".svg")) {
-                                svgName = svgName.replace(".svg", "_" + tablaJSON.getName(j) + ".svg");
-                            } else {
-                                svgName += "_" + tablaJSON.getName(j) + ".svg";
-                            }
-                        }
-                        if (action == Action.saveSvg) {
-                            // AQUI GUARDAMOS EL SVG
-                            try {
-                                GraphGenerator.generateGraphFromFileTmp(engine, dotContent, svgName);
-                                System.out.println("Guardando SVG " + svgName + " with " + engine.toString());
-                            } catch (Exception e) {
-                                System.out.println("Error al guardar el SVG " + svgName);
-                                System.out.println(e.toString());
-                            }
-                        } else if (action == Action.saveDot) {
-                            // AQUI GUARDAMOS EL DOT
-                            try {
-                                FileManager.write(dotName, dotContent);
-                                System.out.println("Guardando DOT " + dotName);
-                            } catch (Exception e) {
-                                System.out.println("Error al guardar el DOT " + dotName);
-                                System.out.println(e.toString());
-                            }
-
-                        } else if (action == Action.saveBoth) {
-                            // AQUI GUARDAMOS AMBOS
-                            try {
-                                //System.out.println("Guardando AMBOS");
-                                System.out.println("Guardando DOT " + dotName);
-                                FileManager.write(dotName, dotContent);
-                            } catch (Exception e) {
-                                System.out.println("Error al guardar el DOT " + dotName);
-                                System.out.println(e.toString());
-                            }
-                            try {
-                                System.out.println("Guardando SVG " + svgName + " with " + engine.toString());
-                                GraphGenerator.generateGraphFromFile(engine, dotName, svgName);
-                            } catch (Exception e) {
-                                System.out.println("Error al guardar el SVG " + svgName);
-                                System.out.println(e.toString());
-                            }
-                        }
-                    }
-                    // System.out.println(tablaJSON.toString());
-                } catch (Exception e) {
-                    System.out.println("ERROR al procesar el archivo JSON " + at.get(i, Content.json));
-                    System.out.println(e.toString());
-                }
-            }
-        }
     }
 }
 
